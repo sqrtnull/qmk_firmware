b05e3a74e91fd720847e5635f825d20b5babc7f1
Jo Ito <sqrtnull@gmail.com>
2024-05-23 16:27:49 +0900

    o

--- keyboards/crkbd/keymaps/sqrtnull/keymap.c
+++ keyboards/crkbd/keymaps/sqrtnull/keymap.c
@@ -18,51 +18,57 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 
-const uint16_t PROGMEM com_sd[] = {KC_S, KC_D,COMBO_END};
-const uint16_t PROGMEM com_sdf[] = {KC_S, KC_D,KC_F,COMBO_END};
-const uint16_t PROGMEM com_df[] = {KC_F, KC_D,COMBO_END};
-const uint16_t PROGMEM com_xc[] = {KC_X, KC_C,COMBO_END};
-const uint16_t PROGMEM com_kl[] = {KC_K, KC_L,COMBO_END};
-const uint16_t PROGMEM com_jk[] = {KC_K, KC_J,COMBO_END};
-const uint16_t PROGMEM com_zxc[] = {KC_Z, KC_X,KC_C,COMBO_END};
-const uint16_t PROGMEM com_cmdt[] = {KC_COMM, KC_DOT,COMBO_END};
-const uint16_t PROGMEM com_cmdtm[] = {KC_M,KC_COMM, KC_DOT,COMBO_END};
+const uint16_t PROGMEM com_sd[]    = {KC_S, KC_D, COMBO_END};
+const uint16_t PROGMEM com_sdf[]   = {KC_S, KC_D, KC_F, COMBO_END};
+const uint16_t PROGMEM com_df[]    = {KC_F, KC_D, COMBO_END};
+const uint16_t PROGMEM com_xc[]    = {KC_X, KC_C, COMBO_END};
+const uint16_t PROGMEM com_kl[]    = {KC_K, KC_L, COMBO_END};
+const uint16_t PROGMEM com_jk[]    = {KC_K, KC_J, COMBO_END};
+const uint16_t PROGMEM com_zxc[]   = {KC_Z, KC_X, KC_C, COMBO_END};
+const uint16_t PROGMEM com_cmdt[]  = {KC_COMM, KC_DOT, COMBO_END};
+const uint16_t PROGMEM com_cmdtm[] = {KC_M, KC_COMM, KC_DOT, COMBO_END};
+const uint16_t PROGMEM com_zeun[] = {KC_0, KC_UNDS, COMBO_END};
 
-combo_t key_combos[]={
-    COMBO(com_sd,OSL(1)), // numbers
-    COMBO(com_kl,KC_ENT), // enter
-    COMBO(com_jk,KC_ESC), // esc
-    COMBO(com_xc,KC_TAB), // tab
-    COMBO(com_df,QK_LEAD), // leader
-    COMBO(com_cmdt,KC_BSPC), // backspace
-    COMBO(com_cmdtm,LALT(KC_BSPC)), // alt-backspace
-    COMBO(com_zxc,KC_CAPS), // caps lock
-    COMBO(com_sdf,MO(4)), // function layer
+combo_t key_combos[] = {
+    COMBO(com_sd, OSL(1)),           // numbers
+    COMBO(com_kl, KC_ENT),           // enter
+    COMBO(com_jk, KC_ESC),           // esc
+    COMBO(com_xc, KC_TAB),           // tab
+    COMBO(com_df, QK_LEAD),          // leader
+    COMBO(com_cmdt, KC_BSPC),        // backspace
+    COMBO(com_cmdtm, LALT(KC_BSPC)), // alt-backspace
+    COMBO(com_zxc, KC_CAPS),         // caps lock
+    COMBO(com_sdf, MO(4)),           // function
+    COMBO(com_zeun, KC_1),           // 1
 };
+
+#define NAV_SCN LT(3,KC_SCLN)
+#define OS_LSFT OSM(MOD_LSFT)
+#define OS_LCTL OSM(MOD_LCTL)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       XXXXXXX,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,LT(3,KC_SCLN), XXXXXXX,
+      XXXXXXX,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, NAV_SCN, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       XXXXXXX,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                      KC_LGUI,OSL(2),OSM(MOD_LSFT),     OSM(MOD_LCTL),KC_SPC,KC_LALT
+                                          KC_LGUI,  OSL(2), OS_LSFT,    OS_LCTL,  KC_SPC, KC_LALT
                                       //`--------------------------'  `--------------------------'
 
   ),
 
     [1] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______,
+      _______, _______, _______, _______, _______, _______,                      _______,    KC_7,    KC_8,    KC_9,    KC_0, _______,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       _______, _______, _______, _______, _______, _______,                      _______,    KC_4,    KC_5,    KC_6,  KC_SPC, _______,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       _______, _______, _______, _______, _______, _______,                      _______,    KC_1,    KC_2,    KC_3, _______, _______,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                         _______, _______, _______,     _______, KC_0, _______
+                                          _______,    KC_1, _______,    _______,    KC_0, _______
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -74,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       _______, KC_TILD,  KC_DLR, KC_LBRC, KC_RBRC, KC_QUOT,                      KC_BSLS, KC_UNDS, KC_LABK, KC_RABK, KC_QUES, _______,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                         _______, _______, _______,     _______, _______, _______
+                                         _______, _______, _______,     _______,    KC_0, _______
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -105,13 +111,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 void leader_start_user(void) {
-    // Do something when the leader key is pressed
 }
 
 void leader_end_user(void) {
     if (leader_sequence_one_key(KC_J)) {
-        SEND_STRING(SS_LCTL(SS_LSFT(SS_LALT(SS_TAP(X_J))))); // MEH J
+        SEND_STRING(SS_LCTL(SS_LSFT(SS_LALT(SS_TAP(X_J)))));
     } else if (leader_sequence_one_key(KC_K)) {
-        SEND_STRING(SS_LCTL(SS_LSFT(SS_LALT(SS_TAP(X_K))))); // MEH K
+        SEND_STRING(SS_LCTL(SS_LSFT(SS_LALT(SS_TAP(X_K)))));
     }
 }
