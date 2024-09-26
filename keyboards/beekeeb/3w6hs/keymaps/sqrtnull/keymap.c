abb53e64cce4b895050b4a6a94156a125b6b2d91
sqrtnull <sqrtnull@pm.me>
2024-09-27 02:17:59 +0900

    o

--- keyboards/beekeeb/3w6hs/keymaps/sqrtnull/keymap.c
+++ keyboards/beekeeb/3w6hs/keymaps/sqrtnull/keymap.c
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, NAV_SCN,
          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,
-                                 KC_LGUI,  MO(2), OS_LSFT,    OS_LCTL,  KC_SPC, KC_LALT
+                      MT(MOD_LGUI,KC_BSPC), MO(2), OS_LSFT,    OS_LCTL,  KC_SPC, MT(MOD_LALT,KC_ENT)
 
   ),
 
