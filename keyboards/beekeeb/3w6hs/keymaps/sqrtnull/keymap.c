c83c5d3bf5d03cab5ced9753fb0751b7fa4d9874
sqrtnull <sqrtnull@pm.me>
2025-05-21 22:49:51 +0900

    feat!: reduce strain on pinky

--- keyboards/beekeeb/3w6hs/keymaps/sqrtnull/keymap.c
+++ keyboards/beekeeb/3w6hs/keymaps/sqrtnull/keymap.c
@@ -41,41 +41,46 @@ combo_t key_combos[] = {
 #define NAV_SCN LT(3,KC_SCLN)
 #define OS_LSFT OSM(MOD_LSFT)
 #define OS_LCTL OSM(MOD_LCTL)
+#define OS_LALT OSM(MOD_LALT)
+#define OS_LGUI OSM(MOD_LGUI)
+
+enum Layer {
+    DEF, NUM, SYM, NAV, FUN
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_split_3x5_3(
+    [DEF] = LAYOUT_split_3x5_3(
          KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, NAV_SCN,
+         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,
          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,
-                                   KC_LGUI, MO(2), OS_LSFT,    OS_LCTL,  KC_SPC, MO(1)
-
+                                 MO(NUM), MO(SYM), OS_LSFT,    OS_LCTL,  KC_SPC, KC_LGUI
   ),
 
-    [1] = LAYOUT_split_3x5_3(
+    [NUM] = LAYOUT_split_3x5_3(
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX,    KC_7,    KC_8,    KC_9, XXXXXXX,
-      XXXXXXX, XXXXXXX, XXXXXXX,   MO(4), XXXXXXX,                      XXXXXXX,    KC_4,    KC_5,    KC_6,    KC_0,
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX,    KC_1,    KC_2,    KC_3, XXXXXXX,
-                                 _______, KC_LALT, _______,    _______, _______, _______
+      XXXXXXX, OS_LGUI, OS_LALT, MO(FUN), XXXXXXX,                      XXXXXXX,    KC_4,    KC_5,    KC_6,    KC_0,
+      XXXXXXX, XXXXXXX, TG(NAV), XXXXXXX, XXXXXXX,                      XXXXXXX,    KC_1,    KC_2,    KC_3, XXXXXXX,
+                                 _______, _______, _______,    _______,    KC_0, KC_LALT
   ),
 
-    [2] = LAYOUT_split_3x5_3(
+    [SYM] = LAYOUT_split_3x5_3(
        KC_GRV,   KC_AT, KC_LPRN, KC_RPRN, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR,  KC_ENT,  KC_ESC,
       KC_EXLM, KC_HASH, KC_LCBR, KC_RCBR, KC_DQUO,                      KC_PIPE, KC_MINS,  KC_EQL, KC_PLUS, KC_COLN,
       KC_TILD,  KC_DLR, KC_LBRC, KC_RBRC, KC_QUOT,                      KC_CAPS, KC_UNDS, KC_BSPC,  KC_TAB, KC_BSLS,
                                  _______, _______, _______,    _______, _______, KC_LALT
   ),
 
-    [3] = LAYOUT_split_3x5_3(
-       KC_ESC, KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_U,                      XXXXXXX, KC_PGUP, KC_HOME, KC_SCLN, XXXXXXX,
-       KC_TAB, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D,                      KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______,
-      KC_WH_L, KC_BTN4, KC_BTN3, KC_BTN5, KC_WH_R,                      XXXXXXX, KC_PGDN,  KC_END, KC_BSPC, XXXXXXX,
-                                 _______, XXXXXXX, _______,    _______, _______, KC_LALT
+    [NAV] = LAYOUT_split_3x5_3(
+       KC_ESC, KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_U,                      KC_HOME, KC_PGUP, TG(NAV), TG(NAV), TG(NAV),
+      TG(NAV), KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D,                      KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, TG(NAV),
+      KC_WH_L, KC_BTN4, KC_BTN3, KC_BTN5, KC_WH_R,                       KC_END, KC_PGDN, KC_BSPC,  KC_TAB, XXXXXXX,
+                                 XXXXXXX, XXXXXXX, _______,    _______, _______, KC_LALT
   ),
 
-    [4] = LAYOUT_split_3x5_3(
+    [FUN] = LAYOUT_split_3x5_3(
       QK_MAKE, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX,   KC_F7,   KC_F8,   KC_F9,  KC_F10,
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX,   KC_F4,   KC_F5,   KC_F6,  KC_F11,
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX,   KC_F1,   KC_F2,   KC_F3,  KC_F12,
-                                 _______, KC_LALT, _______,    _______, _______, _______
+                                 XXXXXXX, XXXXXXX, _______,    _______, _______, KC_LALT
   )
 };
