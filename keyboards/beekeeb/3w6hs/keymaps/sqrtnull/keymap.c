fd28900bb0b886aae10ee93921ee2df3312b5d41
sqrtnull <sqrtnull@pm.me>
2025-05-23 00:42:19 +0900

    feat!: consistency

--- keyboards/beekeeb/3w6hs/keymaps/sqrtnull/keymap.c
+++ keyboards/beekeeb/3w6hs/keymaps/sqrtnull/keymap.c
@@ -45,7 +45,7 @@ combo_t key_combos[] = {
 #define OS_LGUI OSM(MOD_LGUI)
 
 enum Layer {
-    DEF, NUM, SYM, NAV, FUN
+    DEF, NUM, NAV, SYM, FUN, XXX
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -58,29 +58,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [NUM] = LAYOUT_split_3x5_3(
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX,    KC_7,    KC_8,    KC_9, XXXXXXX,
-      XXXXXXX, OS_LGUI, OS_LALT, MO(FUN), XXXXXXX,                      XXXXXXX,    KC_4,    KC_5,    KC_6,    KC_0,
+      XXXXXXX, XXXXXXX, XXXXXXX, MO(FUN), XXXXXXX,                      XXXXXXX,    KC_4,    KC_5,    KC_6,    KC_0,
       XXXXXXX, XXXXXXX, TG(NAV), XXXXXXX, XXXXXXX,                      XXXXXXX,    KC_1,    KC_2,    KC_3, XXXXXXX,
-                                 _______, _______, _______,    _______,    KC_0, KC_LALT
+                                 XXXXXXX, KC_LGUI, _______,    _______,    KC_0, KC_LALT
+  ),
+
+    [NAV] = LAYOUT_split_3x5_3(
+      KC_LGUI, KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_U,                      KC_HOME, KC_PGUP, TG(NAV), TG(NAV), TG(NAV),
+      TG(NAV), KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D,                      KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, TG(NAV),
+      KC_WH_L, KC_BTN4, KC_BTN3, KC_BTN5, KC_WH_R,                       KC_END, KC_PGDN, KC_BSPC,  KC_TAB,  KC_ENT,
+                                 MO(XXX), MO(SYM), _______,    _______,  KC_SPC, KC_LALT
   ),
 
     [SYM] = LAYOUT_split_3x5_3(
        KC_GRV,   KC_AT, KC_LPRN, KC_RPRN, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR,  KC_ENT,  KC_ESC,
       KC_EXLM, KC_HASH, KC_LCBR, KC_RCBR, KC_DQUO,                      KC_PIPE, KC_MINS,  KC_EQL, KC_PLUS, KC_COLN,
       KC_TILD,  KC_DLR, KC_LBRC, KC_RBRC, KC_QUOT,                      KC_CAPS, KC_UNDS, KC_BSPC,  KC_TAB, KC_BSLS,
-                                 _______, _______, _______,    _______, _______, KC_LALT
-  ),
-
-    [NAV] = LAYOUT_split_3x5_3(
-       KC_ESC, KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_U,                      KC_HOME, KC_PGUP, TG(NAV), TG(NAV), TG(NAV),
-      TG(NAV), KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D,                      KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, TG(NAV),
-      KC_WH_L, KC_BTN4, KC_BTN3, KC_BTN5, KC_WH_R,                       KC_END, KC_PGDN, KC_BSPC,  KC_TAB, XXXXXXX,
-                                 XXXXXXX, XXXXXXX, _______,    _______, _______, KC_LALT
+                                 KC_LGUI, XXXXXXX, _______,    _______,  KC_SPC, KC_LALT
   ),
 
     [FUN] = LAYOUT_split_3x5_3(
       QK_MAKE, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX,   KC_F7,   KC_F8,   KC_F9,  KC_F10,
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX,   KC_F4,   KC_F5,   KC_F6,  KC_F11,
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX,   KC_F1,   KC_F2,   KC_F3,  KC_F12,
-                                 XXXXXXX, XXXXXXX, _______,    _______, _______, KC_LALT
+                                 XXXXXXX, KC_LGUI, _______,    _______,  KC_SPC, KC_LALT
+  ),
+
+    [XXX] = LAYOUT_split_3x5_3(
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                                 XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX
   )
 };
